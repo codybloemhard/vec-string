d809b7ed19037b40690d18ebe0289f90e95d885a
Cody Bloemhard <cblmhrd@live.nl>
2021-10-03 20:59:31 +0200

    fix

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # vec-string
 Very short crate to display vectors Vec&lt;T> where T: Display
+Normally you can do `format!("{:?}", vec);` when the elements implement `Debug`.
+This crate is to do the same but for when the elements implement `Display`.
+```rust
+assert_eq!("[1, 2, 3]", vec![1, 2, 3].vec_string());
+```
--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,9 @@ pub trait VecString{
     fn vec_string(&self) -> String;
 }
 
+/// Get string of Vec<T> where T: Display
 impl<T> VecString for Vec<T> where T: std::fmt::Display{
+    /// assert_eq!("[1, 2, 3]", vec![1, 2, 3].vec_string());
     fn vec_string(&self) -> String{
         let mut string = String::new();
         for x in self{
