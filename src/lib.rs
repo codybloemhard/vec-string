e38566ae56c58c60079a424a3f4d6046a3684772
Cody Bloemhard <codybloemhard@gmail.com>
2022-11-22 20:04:26 +0100

    added: vec_string_inner; version: v0.2.0;

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,39 @@
 pub trait VecString{
     fn vec_string(&self) -> String;
+    fn vec_string_inner(&self) -> String;
 }
 
 /// Get string of Vec<T> where T: Display
 impl<T> VecString for Vec<T> where T: std::fmt::Display{
     /// assert_eq!("[1, 2, 3]", vec![1, 2, 3].vec_string());
     fn vec_string(&self) -> String{
+        format!("[{}]", self.vec_string_inner())
+    }
+
+    /// assert_eq!("1, 2, 3", vec![1, 2, 3].vec_string());
+    fn vec_string_inner(&self) -> String{
         let mut string = String::new();
         for x in self{
             string.push_str(&format!("{}, ", x));
         }
         string.pop();
         string.pop();
-        format!("[{}]", string)
+        string
     }
 }
 
 #[cfg(test)]
 mod tests {
     use crate::VecString;
+
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
     }
+
     #[test]
     fn test() {
         assert_eq!("[1, 2, 3]", vec![1, 2, 3].vec_string());
+        assert_eq!("1, 2, 3", vec![1, 2, 3].vec_string_inner());
     }
 }
